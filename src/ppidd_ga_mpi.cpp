da91f81367db0515a93a307f689c843ff46cc7f0
Andy May <ajmay81@gmail.com>
2025-04-25 14:18:52 +0100

    Zero comes from PPIDD_FORTINT

--- src/ppidd_ga_mpi.cpp
+++ src/ppidd_ga_mpi.cpp
@@ -422,7 +422,7 @@ static int n_in_msg_mpiq=0;
         //else if (! PPIDD_Nxtval_initialised) {
         /* first call needs to be collective and will return 0*/
 	std::string name="Nxtval";
-        PPIDD_Create(&name[0],1,0,1,&PPIDD_Nxtval_handle);
+        PPIDD_Create(&name[0],1,PPIDD_FORTINT,1,&PPIDD_Nxtval_handle);
         PPIDD_Zero(PPIDD_Nxtval_handle);
         PPIDD_Nxtval_initialised=1;
         return 0;
